61927cc3feb3a663b7f5c9f23f78805f03d0f1c9
yusril-helloworld <yusrilhelloworld@gmail.com>
2026-08-14 04:46:06 +0800

    Switch CSS import to style.css to bypass git cache

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 // app/layout.tsx
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
-import "./app/globals.css";
+import "./style.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
