991f06453b15a1ffaffc776498ed50b43fad6281
yusril-helloworld <yusrilhelloworld@gmail.com>
2026-08-14 04:43:18 +0800

    Fix nested app folder structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 // app/layout.tsx
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./app/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
