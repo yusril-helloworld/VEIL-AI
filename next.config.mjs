99c84c1f628ee570fd34e4996861669ded149457
yusril-helloworld <yusrilhelloworld@gmail.com>
2026-08-14 04:02:56 +0800

    Add next.config.mjs for PWA build

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
+/** @type {import('next').NextPage} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default withPWA(nextConfig);
